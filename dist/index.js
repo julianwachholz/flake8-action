096c8c45f46f46115399158496cff03e3acfb0e6
Julian Wachholz <julian@wachholz.ch>
2021-09-23 11:35:24 +0200

    Add parameter to install plugins
    Fixes #22

--- dist/index.js
+++ dist/index.js
@@ -14,7 +14,8 @@ const parseFlake8Output = __nccwpck_require__(586);
 const { GITHUB_TOKEN } = process.env;
 
 async function installFlake8() {
-  await exec.exec("pip install flake8");
+  const plugins = core.getInput("plugins");
+  await exec.exec(`pip install flake8 ${plugins}`);
 }
 
 async function runFlake8() {
